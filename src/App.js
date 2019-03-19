190b311543df56ac2f8749a50a61f80440a3ef38
Josh Hepworth <jrhepworth12@gmail.com>
2019-03-19 15:41:20 -0500

    more testing for github

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-    <h1> Country Naturals </h1>
+    <h1> Country Naturals Geneva </h1>
       </div>
     );
   }
