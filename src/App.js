c544992502f9cd94e6638cd069d00b5be9425be9
Josh Hepworth <jrhepworth12@gmail.com>
2019-03-20 12:58:46 -0500

    inserted logo and added material ui components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Icon from '@material-ui/core/Icon';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    <h1> Country Naturals Geneva </h1>
+    <img className="logo" src="https://i.imgur.com/uXNMwVtl.jpg"/>
       </div>
     );
   }
